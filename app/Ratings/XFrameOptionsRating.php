8febab1a03a83e1153884bc2c4c47bfff49923f4
Sascha Brendel <sascha.brendel@hackmanit.de>
2017-01-12 17:08:47 +0100

    Added X-Frame-Options.

---
+++ app/Ratings/XFrameOptionsRating.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Ratings;
+
+use App\HTTPResponse;
+
+class XFrameOptionsRating implements Rating
+{
+
+    protected $url;
+    protected $rating;
+    protected $comment;
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+        $this->rate();
+    }
+
+    protected function rate()
+    {
+        $header = $this->getHeader();
+
+        if ($header === null) {
+            $this->rating   = 'C';
+            $this->comment  = 'X-Frame-Options header is not set.';
+        }
+
+        elseif (count($header) > 1) {
+            $this->rating   = 'C';
+            $this->comment  = 'X-Frame-Options header is set multiple times.';
+        }
+
+        else {
+            $header = $header[0];
+
+            $this->rating   = 'A';
+            $this->comment  = 'X-Frame-Options header is set and does not contain any wildcard.';
+
+            if (strpos($header, '*') !== false) {
+                $this->rating   = 'C';
+                $this->comment  = 'Header contains a wildcard and is thereby useless.';
+            }
+        }
+    }
+
+    public static function getDescription()
+    {
+        // OWASP Secure Headers Project
+        // https://www.owasp.org/index.php/OWASP_Secure_Headers_Project#X-Frame-Options
+        return 'X-Frame-Options response header improve the protection of web applications against Clickjacking. It declares a policy communicated from a host to the client browser on whether the browser must not display the transmitted content in frames of other web pages.';
+    }
+
+    public static function getBestPractice()
+    {
+        // Hackmanit
+        return 'Best Practice is to set this header accordingly to your needs. Do not use "allow-from: *".';
+    }
+
+    public function getHeader()
+    {
+        return HTTPResponse::get($this->url)->getHeaders()->get("X-Frame-Options");
+    }
+
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    public function getComment()
+    {
+        return $this->comment;
+    }
+}
