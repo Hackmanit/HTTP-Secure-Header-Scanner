4a7382e8f96829dcc5b55f418d1f53db55a0e0e3
Sascha Brendel <sascha.brendel@hackmanit.de>
2017-01-17 15:09:34 +0100

    Implemented URL specific rating as App\Report.

--- app/Report.php
+++ app/Report.php
@@ -2,32 +2,108 @@
 
 namespace App;
 
+use App\Ratings;
 
-use Illuminate\Support\Facades\Redis;
-
-
-/*
- * Was hab ich eigentlich vor damit?
+/**
+ * Returns a Report / Rating for the given URL.
  *
+ * @property Ratings\ContentTypeRating ContentTypeRating
+ * @property Ratings\CSPRating ContentSecurityPolicyRating
+ * @property Ratings\HPKPRating HttpPublicKeyPinningRating
+ * @property Ratings\HSTSRating HttpStrictTransportSecurityRating
+ * @property Ratings\XContentTypeOptionsRating XContentTypeOptionsRating
+ * @property Ratings\XFrameOptionsRating XFrameOptionsRating
+ * @property Ratings\XXSSProtectionRating XXSSProtectionRating
  */
-
 class Report
 {
-    protected $id;
-    protected $crawledUrls;
-    protected $overAllRating;
 
-    /**
-     * Report constructor.
-     * @param $id
-     */
-    public function __construct($id)
+    public $url = null;
+    public $status = 'error';
+    public $siteRating = 'C';
+    public $comment = '';
+
+    public function __construct($url)
     {
-        $this->id = $id;
-        $this->crawledUrls = Redis::hget($id, "crawledUrls");
-
-        Redis::hset($id, "reportFinished", true);
+        $this->url = $url;
+        $this->doRatings();
+        $this->doSiteRating();
+        $this->status = 'success';
     }
 
+    /**
+     * Rate the specific headers.
+     */
+    protected function doRatings()
+    {
+        $this->ContentSecurityPolicyRating       = new Ratings\CSPRating( $this->url );
+        $this->ContentTypeRating                 = new Ratings\ContentTypeRating( $this->url );
+        $this->HttpPublicKeyPinningRating        = new Ratings\HPKPRating( $this->url );
+        $this->HttpStrictTransportSecurityRating = new Ratings\HSTSRating( $this->url );
+        $this->XContentTypeOptionsRating         = new Ratings\XContentTypeOptionsRating( $this->url );
+        $this->XFrameOptionsRating               = new Ratings\XFrameOptionsRating( $this->url );
+        $this->XXSSProtectionRating              = new Ratings\XXSSProtectionRating( $this->url );
+    }
 
+    /**
+     * Rate the site's security.
+     */
+    protected function doSiteRating()
+    {
+        $this->siteRating = 'C';
+        $this->comment = 'The site is insecure.';
+
+        /*
+         * Criteria for H
+         *
+         * All Ratings with grade A
+         */
+        if (
+            (strpos($this->ContentSecurityPolicyRating->getRating()      , 'A')  !== false) &&
+            (strpos($this->ContentTypeRating->getRating()                , 'A')  !== false) &&
+            (strpos($this->HttpPublicKeyPinningRating->getRating()       , 'A')  !== false) &&
+            (strpos($this->HttpStrictTransportSecurityRating->getRating(), 'A')  !== false) &&
+            (strpos($this->XContentTypeOptionsRating->getRating()        , 'A')  !== false) &&
+            (strpos($this->XFrameOptionsRating->getRating()              , 'A')  !== false) &&
+            (strpos($this->XXSSProtectionRating->getRating()             , 'A')  !== false)
+        ) {
+            $this->siteRating = 'H';
+            $this->comment = 'WOHA! Great work! Everything is perfect!';
+        }
+
+        /*
+         * Criteria for A
+         */
+        elseif (
+            (strpos($this->HttpStrictTransportSecurityRating->getRating(), 'A')  !== false) &&
+            (strpos($this->XXSSProtectionRating->getRating()             , 'A')  !== false) &&
+            ((strpos($this->ContentSecurityPolicyRating->getRating(), 'B')  !== false) || (strpos($this->ContentSecurityPolicyRating->getRating(), 'A')  !== false)) &&
+            (strpos($this->ContentTypeRating->getRating()                , 'A')  !== false) &&
+            (strpos($this->XContentTypeOptionsRating->getRating()        , 'A')  !== false) &&
+            (strpos($this->XFrameOptionsRating->getRating()              , 'A')  !== false)
+        ) {
+            $this->siteRating = 'A';
+            $this->comment = 'The site is secure.';
+
+        }
+
+        /*
+         * Criteria for B
+         */
+        elseif (
+            ((strpos($this->HttpStrictTransportSecurityRating->getRating(), 'B')  !== false) || (strpos($this->HttpStrictTransportSecurityRating->getRating(), 'A')  !== false)) &&
+            ((strpos($this->ContentSecurityPolicyRating->getRating(), 'B')  !== false) || (strpos($this->ContentSecurityPolicyRating->getRating(), 'A')  !== false)) &&
+            ((strpos($this->ContentTypeRating->getRating(), 'B') !== false) || (strpos($this->ContentTypeRating->getRating(), 'A') !== false)) &&
+            (strpos($this->XContentTypeOptionsRating->getRating(), 'A')  !== false) &&
+            (strpos($this->XFrameOptionsRating->getRating(), 'A') !== false)
+        ) {
+            $this->siteRating = 'B';
+            $this->comment = 'The site is secure.';
+        }
+    }
+
+    public function __toString()
+    {
+        return json_encode($this, JSON_PRETTY_PRINT);
+    }
 }
