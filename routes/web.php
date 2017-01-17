3a67e68db541514baef9ea685503af5993d8cde1
Sascha Brendel <sascha.brendel@hackmanit.de>
2017-01-17 15:14:41 +0100

    Modified routes.

--- routes/web.php
+++ routes/web.php
@@ -15,34 +15,3 @@ Route::get('/', 'HeaderController@index');
 Route::post('/', ['as' => 'requestReport', 'uses' => 'HeaderController@requestReport']);
 
 Route::get('/jsConfig', 'HeaderController@jsConfig');
-Route::get('report/{id}', ['as' => 'displayReport', 'uses' => 'HeaderController@displayReport']);
-
-Route::get('/test', function() {
-    $id = str_random();
-    $crawler = new App\Crawler($id, "https://www.hackmanit.de/", collect(["hackmanit.de", "www.hackmanit.de"]), collect(['customJson' => '{"a": "href"}']), 1000);
-    $crawler->extractAllLinks();
-    dd($crawler);
-});
-
-Route::get('/testHSTS', function() {
-    dd((new App\Ratings\HSTSRating("http://www.hackmanit.de/"))->getRating());
-});
-
-Route::get('/testUnparseUrl', function () {
-    function unparse_url($parsed_url, $scanned_url)
-    {
-        $scheme = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : 'http://';
-        $host = isset($parsed_url['host']) ? strtolower($parsed_url['host']) : strtolower(parse_url($scanned_url, PHP_URL_HOST));
-        $port = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
-        $user = isset($parsed_url['user']) ? $parsed_url['user'] : '';
-        $pass = isset($parsed_url['pass']) ? ':' . $parsed_url['pass'] : '';
-        $pass = ($user || $pass) ? "$pass@" : '';
-        $path = isset($parsed_url['path']) ? $parsed_url['path'] : '';
-        if (strncmp($path, "/", 1) !== 0) $path = "/" . $path;
-        $query = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
-        return "$scheme$user$pass$host$port$path$query";
-    }
-
-    $parsed_url = parse_url('//www.hackmanit.de/demo/sichere-webentwicklung-teaser.pdf');
-    return unparse_url($parsed_url, 'www.hackmanit.de');
-});
