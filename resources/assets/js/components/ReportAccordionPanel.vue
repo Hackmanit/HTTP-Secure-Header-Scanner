428f7d74dd3bb7715f2862fde6839b4631d50e87
Sascha Brendel <sascha.brendel@hackmanit.de>
2017-02-09 13:50:56 +0100

    Added todos.

--- resources/assets/js/components/ReportAccordionPanel.vue
+++ resources/assets/js/components/ReportAccordionPanel.vue
@@ -3,6 +3,7 @@
         <div class="panel panel-default">
             <div class="panel-heading" role="tab" :id="'heading' + header">
                 <h4 class="panel-title">
+                    <!-- TODO: label styling in relation to the rating -->
                     <span class="label label-default">{{ rating[header] }}</span>
                     <a role="button" data-toggle="collapse" data-parent="#accordion" :href="'#collapse' + header" aria-expanded="false" :aria-controls="'collapse' + header">
                         {{ header }}
@@ -17,7 +18,7 @@
                             <th>Rating</th>
                             <th>Site</th>
                         </tr>
-
+                        <!-- TODO: label styling in relation to the rating -->
                         <tr v-for='site in fullreport[ header ]'>
                             <td><span class="label label-default">{{ site.rating }}</span></td>
                             <td><a :href="'/single?url=' + site.url">{{ site.url }}</a></td>
