f67e4d47960a16ab3169bbd392fdd52b56312b68
Sascha Brendel <mail@lednerb.eu>
2018-09-13 23:19:56 +0200

    Simplified callbackUrl test.

--- tests/Feature/ScanStartTest.php
+++ tests/Feature/ScanStartTest.php
@@ -5,18 +5,10 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-// use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Log;
 
 class ScanStartTest extends TestCase
 {
-
-    public function setUp() {
-        parent::setUp();
-
-        $this->app->configureMonologUsing((function ($monolog) {
-            $monolog->pushHandler(new \Monolog\Handler\TestHandler());
-        }));
-    }
 
     /** @test */
     public function a_header_scan_can_be_started_if_the_correct_parameters_are_sent() {
@@ -72,22 +64,15 @@ class ScanStartTest extends TestCase
     /** @test */
     public function if_a_callbackurl_is_not_reachable_it_will_be_logged()
     {
+        Log::shouldReceive('warning')
+            ->with('Could not send the report to the following callback url: http://localhost:9002')
+            ->once();
 
         $response = $this->json('POST', '/api/v1/header', [
             'url' => 'https://siwecos.de',
             'dangerLevel' => 0,
             "callbackurls" => ["http://localhost:9002"]
         ]);
-
-        // Retrieve the records from the Monolog TestHandler
-        $records = \Log::getMonolog()->getHandlers()[0]->getRecords();
-
-        $this->assertEquals(
-            'Could not send the report to the following callback url: http://localhost:9002',
-            $records[0]['message']
-        );
-        // \Log::shouldReceive('warning')->once();
-
 
         $response->assertStatus(200);
     }
